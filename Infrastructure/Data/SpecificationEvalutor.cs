8213592bfe14285d31bc0d4aa37adbb6105d9cb0
Mohamed <mohamedshehatadev17@gmail.com>
2026-05-17 23:23:36 +0300

    add specification pattern

---
+++ Infrastructure/Data/SpecificationEvalutor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data
+{
+    public class SpecificationEvalutor<T> where T : BaseEntity
+    {
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> spec)
+            {
+                var query = inputQuery;
+    
+                if (spec.Criteria != null)
+                {
+                    query = query.Where(spec.Criteria);
+                }
+    
+                return query;
+        }
+    }
+}
